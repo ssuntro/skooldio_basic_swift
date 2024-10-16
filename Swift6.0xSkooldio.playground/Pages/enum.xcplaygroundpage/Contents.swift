6cfbfcf9cfa4e190776884bffa5a1418d6a2be51
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-16 19:29:34 +0700

    enum

--- Swift6.0xSkooldio.playground/Pages/enum.xcplaygroundpage/Contents.swift
+++ Swift6.0xSkooldio.playground/Pages/enum.xcplaygroundpage/Contents.swift
@@ -1,3 +1,25 @@
+// plain enum
+enum Direction {
+    case north, south, east, west
+
+    func toString() -> String {
+        switch self {
+        case .east:
+            return "east"
+        case .north:
+            return "north"
+        case .south:
+            return "south"
+        case .west:
+            return "west"
+        }
+    }
+}
+// Direction.east.rawValue
+// Direction.init(rawValue: "east")
+
+
+// enum raw value
 enum Planet: String {
     case mercury , venus, earth, mars
 }
