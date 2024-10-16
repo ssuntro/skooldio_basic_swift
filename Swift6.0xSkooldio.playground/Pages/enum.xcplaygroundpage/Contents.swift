7085e02b8b43c6a910558942227ae5fc0074a589
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2024-10-16 19:59:02 +0700

    enum

--- Swift6.0xSkooldio.playground/Pages/enum.xcplaygroundpage/Contents.swift
+++ Swift6.0xSkooldio.playground/Pages/enum.xcplaygroundpage/Contents.swift
@@ -24,3 +24,54 @@ enum Planet: String {
     case mercury , venus, earth, mars
 }
 print(Planet.mercury.rawValue)
+
+
+// ------------------------------------------------------
+//why protocol?
+// 1. Polymorphism without inheritance
+protocol PinRule {
+func evaluate() -> Bool
+}
+class DuplicateDigit: PinRule {
+    func evaluate() -> Bool {
+        return true
+    }
+}
+enum SequenceDigit: PinRule {
+    case ascending
+    func evaluate() -> Bool {
+        return false
+    }
+    init() { self = .ascending }
+}
+struct LessThanSixDigit: PinRule {
+    func evaluate() -> Bool {
+        return true
+    }
+}
+let rules: [PinRule] = [DuplicateDigit(), SequenceDigit(), LessThanSixDigit()]
+rules.reduce(true) { partialResult, rule  in
+    rule.evaluate()
+}
+func isValidPin(with rule: PinRule) {
+//    ... สนใจเฉพาะ rule.evaluate()
+}
+
+
+// 2. Ease to double
+struct PinScreen {
+    init(rule: PinRule) {
+        // Double inject ง่าย ทำให้test ง่าย maintain ง่าย
+    }
+}
+class SpyPinRule: PinRule {
+    var numberOfCalled = 0
+    func evaluate() -> Bool {
+        numberOfCalled = numberOfCalled + 1
+        return true
+    }
+}
+let spyRule = SpyPinRule()
+PinScreen(rule: spyRule)
+spyRule.numberOfCalled
+
