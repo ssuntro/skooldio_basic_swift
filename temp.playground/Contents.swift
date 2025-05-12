ebbfeac1f67df68914a37661febcca157f2f6964
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2025-05-12 22:22:43 +0700

    temp (value type vs ref type)

--- temp.playground/Contents.swift
+++ temp.playground/Contents.swift
@@ -1,16 +1,19 @@
 import UIKit
 
 //review again.
+//GOAL: DEMO VAR/LET + VALUE TYPE VS REF TYPE
+//key message: ถึงเปน let หมด ก้ยังค่าเปลี่ยนได้เพราะ ref type
 //definition of var vs let.
 //"immutable by default" concept in sturct
+// ref type in swift = closure, function, and class
 struct ExampleStruct {
     let a: Double  //struct
     let b: NetworkError
     let c: String  //struct
-    let d: [String]  // array of struct
+//    let d: [String]  // array of struct
     let e: UIView?  // optional class
     let rules: [(_ pin: String) -> Bool]
-    let f: [String: Int]  //dictionary
+    var rule: (_ pin: String) -> Bool
 }
 
 enum NetworkError: Error {
@@ -18,23 +21,49 @@ enum NetworkError: Error {
     case timeout
 }
 
-let pinRules: [(_ pin: String) -> Bool] = [
+var pinRules: [(_ pin: String) -> Bool] = [
     { pin in pin.count == 4 },                        // Rule 1: must be exactly 4 characters
     { pin in pin.allSatisfy(\.isNumber) },            // Rule 2: must contain only digits
     { pin in Set(pin).count == pin.count }            // Rule 3: must not contain duplicate digits
 ]
+var rule: (_ pin: String) -> Bool = { pin in return true }
 let aa = ExampleStruct(
     a: 9,
     b: .timeout,
     c: "",
-    d: ["", ""],
-    e: nil,
+//    d: ["", ""],
+    e: UIView(), //nil
     rules: pinRules,
-    f: ["key": 11]
+    rule: rule
 )
 
 //aa.a = 11 //compile-time error
 //aa.b = .noConnection //compile-time error
 
+//aa.d.append("") //to prevent compiler-error. aa and d need to be var
+aa.e!.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//aa.rules.append({ _ in true }) //compile-time error due to array
+pinRules[0] = { pin in return false }
+aa.rules[0]("3333")
+//aa.rule = { pin in return false } //compile-time error
 
 
+
+
+//MARK: function and closure to demo
+
+// A closure that captures and modifies a variable
+func makeCounter() -> () -> Int {
+    var count = 0
+    return {
+        count = count + 1
+        return count
+    }
+}
+
+let counterA = makeCounter()
+let counterB = counterA // same reference
+
+print(counterA()) // 1
+print(counterB()) // 2
+print(counterA()) // 3
